0f9acf59b438acbf668d352616d44751a26d2485
inti <intirocks888@gmail.com>
2020-01-23 15:40:14 -0600

    Revert "operadores logicos"
    This reverts commit 92259ce38d67a6b67119ac5bdd9cbdbee3e92aff.

--- src/arbol/Operacion.java
+++ src/arbol/Operacion.java
@@ -27,9 +27,7 @@ public class Operacion implements Instruccion{
         CADENA,
         MAYOR_QUE,
         MENOR_QUE,
-        CONCATENACION,
-        AND,
-        OR
+        CONCATENACION
     }
     /**
      * Tipo de operación a ejecutar.
@@ -124,10 +122,6 @@ public class Operacion implements Instruccion{
             return ((Double)operadorIzq.ejecutar(ts)).doubleValue()<((Double)operadorDer.ejecutar(ts)).doubleValue();
         }else if(tipo== Tipo_operacion.CONCATENACION){
             return operadorIzq.ejecutar(ts).toString()+operadorDer.ejecutar(ts).toString();
-        }else if(tipo==Tipo_operacion.AND){
-            return ((Boolean)operadorIzq.ejecutar(ts))&&((Boolean)operadorDer.ejecutar(ts));
-        }else if(tipo == Tipo_operacion.OR){
-            return ((Boolean)operadorIzq.ejecutar(ts))||((Boolean)operadorDer.ejecutar(ts));
         }else{
             return null;
         }
